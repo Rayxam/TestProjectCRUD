9eaad496279f2a75fb29b02212c1c3af82a042c6
Александр <Rayxam@gmail.com>
2023-01-15 15:55:37 +0300

    Lesson #41

--- src/main/java/ru/testproject/spring/controllers/PeopleController.java
+++ src/main/java/ru/testproject/spring/controllers/PeopleController.java
@@ -8,15 +8,18 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.testproject.spring.dao.PersonDAO;
 import ru.testproject.spring.models.Person;
+import ru.testproject.spring.util.PersonValidator;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
     private final PersonDAO personDAO;
+    private final PersonValidator personValidator;
 
     @Autowired
-    public PeopleController(PersonDAO personDAO) {
+    public PeopleController(PersonDAO personDAO, PersonValidator personValidator) {
         this.personDAO = personDAO;
+        this.personValidator = personValidator;
     }
 
     @GetMapping("")
@@ -37,7 +40,9 @@ public class PeopleController {
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult) {
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+        personValidator.validate(person, bindingResult);
 
         if(bindingResult.hasErrors())
             return "people/new";
@@ -55,6 +60,7 @@ public class PeopleController {
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("person") @Valid Person person,
                          BindingResult bindingResult, @PathVariable("id") int id) {
+        personValidator.validate(person, bindingResult);
 
         if (bindingResult.hasErrors())
             return "people/edit";
